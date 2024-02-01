1e9d985a43df0940b1322a3ed6039e322474704f
Corentin R <76619184+Koroh63@users.noreply.github.com>
2024-02-01 09:29:13 +0100

    selecting models whithout conversion to number of values

--- src/main.py
+++ src/main.py
@@ -1,8 +1,42 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from utils import * 
+
+
 def main(): 
-    print("hey there") 
-  
+    fullDataSet = importCleanDataSet()
+
+    getInfoDataSet(fullDataSet)
+
+
+    X,Y = separateValues(fullDataSet)
+    
+    Xtrain,Xtest,ytrain,ytest = initTraining(X,Y)
+
+    modelLinearRegression = LinearRegression()
+    modelLasso = Lasso()
+    modelElasticNet = ElasticNet()
+
+    modelLinearRegression.fit(Xtrain,ytrain)
+    modelLasso.fit(Xtrain,ytrain)
+    modelElasticNet.fit(Xtrain,ytrain)
+
+    ypreditLineatRegression = modelLinearRegression.predict(Xtest)
+    ypreditLasso = modelLasso.predict(Xtest)
+    ypreditElasticNet = modelElasticNet.predict(Xtest)
+    # print(ypredit)
+    print(accuracy_score(ytest, ypreditLineatRegression))
+    print(accuracy_score(ytest, ypreditLasso))
+    print(accuracy_score(ytest, ypreditElasticNet))
+
+    
+    
+
+
   
 # Using the special variable  
 # __name__ 
 if __name__=="__main__": 
     main() 
+
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,43 @@
-DATASET_LOCATION = "CSV/1900_2021_DISASTERS.csv"
+DATASET_LOCATION = "../CSV/1900_2021_DISASTERS.csv"
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-def importDataSet():
-    return pd.read_csv(DATASET_LOCATION) 
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+def importCleanDataSet():
+    ds = pd.read_csv(DATASET_LOCATION,skipinitialspace=True,usecols=[0,4,5,6,8,10,11,12,13,14,23,24,25,27,28,29,30,31,32,33,34,35,37]) 
     
+    return ds
+
+def getInfoDataSet(ds):
+    ds.info()
+
+def separateValues(ds):
+    x = ds.drop(['Total Deaths'],axis=1)
+    y = ds['Total Deaths'].values
+    return x,y
+
+def initTraining(x,y):
+    Xtrain, Xtest, ytrain, ytest = train_test_split(x, y,test_size=0.25, random_state=0)
+    Xtrain = Xtrain.values
+    Xtest = Xtest.values
+    if len(Xtrain.shape) < 2:
+        Xtrain = Xtrain.reshape(-1, 1)
+    if len(Xtest.shape) < 2:
+        Xtest = Xtest.reshape(-1, 1)
+
+    return Xtrain,Xtest,ytrain,ytest
+
+def showHist(X):
+    index = np.arange(len(X))
+    bar_width = 0.9
+    
+    plt.bar(index, X[1], bar_width,  color="green")
+    plt.xticks(index, X[0]) # labels get centered
+    plt.show()
