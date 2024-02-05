f0bec3593cf6f91fc4f8abe65ab6b39327f6e0a0
Corentin R <corentin.richard@etu.uca.fr>
2024-02-05 17:19:11 +0100

    Cleaning Data

--- src/main.py
+++ src/main.py
@@ -4,31 +4,32 @@ from sklearn.linear_model import ElasticNet
 from utils import * 
 
 
+
 def main(): 
     fullDataSet = importCleanDataSet()
 
     getInfoDataSet(fullDataSet)
 
 
-    # X,Y = separateValues(fullDataSet)
+    X,Y = separateValuesTD(fullDataSet)
     
-    # Xtrain,Xtest,ytrain,ytest = initTraining(X,Y)
+    Xtrain,Xtest,ytrain,ytest = initTraining(X,Y)
 
-    # modelLinearRegression = LinearRegression()
-    # modelLasso = Lasso()
-    # modelElasticNet = ElasticNet()
+    modelLinearRegression = LinearRegression()
+    modelLasso = Lasso(alpha=5.0,max_iter=100000)
+    modelElasticNet = ElasticNet(alpha=5.0,max_iter=100000)
 
-    # modelLinearRegression.fit(Xtrain,ytrain)
-    # modelLasso.fit(Xtrain,ytrain)
-    # modelElasticNet.fit(Xtrain,ytrain)
+    #modelLinearRegression.fit(Xtrain,ytrain)
+    modelLasso.fit(Xtrain,ytrain)
+    modelElasticNet.fit(Xtrain,ytrain)
 
-    # ypreditLineatRegression = modelLinearRegression.predict(Xtest)
-    # ypreditLasso = modelLasso.predict(Xtest)
-    # ypreditElasticNet = modelElasticNet.predict(Xtest)
-    # # print(ypredit)
-    # print(accuracy_score(ytest, ypreditLineatRegression))
-    # print(accuracy_score(ytest, ypreditLasso))
-    # print(accuracy_score(ytest, ypreditElasticNet))
+    #ypreditLineatRegression = modelLinearRegression.predict(Xtest)
+    ypreditLasso = modelLasso.predict(Xtest)
+    ypreditElasticNet = modelElasticNet.predict(Xtest)
+    # print(ypredit)
+    #print(accuracy_score(ytest, ypreditLineatRegression))
+    print(accuracy_score(ytest, ypreditLasso))
+    print(accuracy_score(ytest, ypreditElasticNet))
 
     
     
--- src/utils.py
+++ src/utils.py
@@ -11,15 +11,78 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 def importCleanDataSet():
-    ds = pd.read_csv(DATASET_LOCATION,skipinitialspace=True,usecols=[0,4,5,6,8,10,11,12,28,29,30,31,32,33,34,35,37]) 
     
+    ## Read DataFrame ##
+    ds = pd.read_csv(DATASET_LOCATION,skipinitialspace=True,usecols=[0,4,5,6,8,10,11,12,28,29,30,31,32,33,34,35,37]) 
+
+    ## Manage Date ##
+    ds.fillna({'Start Month': 1, 'Start Day': 1,'End Month':1,'End Day':1,'Total Deaths':0,'No Injured':0,'No Affected':0,'Total Affected':0}, inplace=True)
+    
+    ds['Start Day'] = ds['Start Day'].astype(int)
+    ds['End Day'] = ds['End Day'].astype(int)
+    ds['Start Month'] = ds['Start Month'].astype(int)
+    ds['End Month'] = ds['End Month'].astype(int)
+    ds['Year'] = ds['Year'].astype(int)
+    ds['End Year'] = ds['End Year'].astype(int)
+
+    dsStartTmp = pd.DataFrame()
+    dsEndTmp = pd.DataFrame()
+
+    dsStartTmp['year'] = ds['Year']
+    dsStartTmp['month'] = ds['Start Month']
+    dsStartTmp['day'] = ds['Start Day']
+    
+    dsEndTmp['year'] = ds['End Year']
+    dsEndTmp['month'] = ds['End Month']
+    dsEndTmp['day'] = ds['End Day']
+
+    ds['Start Date'] = pd.to_datetime(dsStartTmp[['year', 'month', 'day']], errors='coerce')
+    ds['End Date'] = pd.to_datetime(dsEndTmp[['year', 'month', 'day']], errors='coerce')
+
+    # Fill NaT values with a default date (you can change this to fit your needs)
+    default_date = pd.to_datetime('1900-01-01')
+    ds['Start Date'].fillna(default_date, inplace=True)
+    ds['End Date'].fillna(default_date, inplace=True)
+
+    ds['Duration'] = (ds['End Date'] - ds['Start Date']).dt.days
+    ds.drop(['Start Month', 'Start Day','End Year','End Month','End Day','Start Date','End Date','Event Name'],axis=1,inplace=True)
+
+    ## Mapping String values ## 
+
+    type_mapping = {type_str: idx + 1 for idx, type_str in enumerate(ds['Disaster Type'].unique())}
+    ds['Disaster Type'] = ds['Disaster Type'].replace(type_mapping)
+
+    type_mapping = {type_str: idx + 1 for idx, type_str in enumerate(ds['Disaster Subgroup'].unique())}
+    ds['Disaster Subgroup'] = ds['Disaster Subgroup'].replace(type_mapping)
+
+    type_mapping = {type_str: idx + 1 for idx, type_str in enumerate(ds['Disaster Subtype'].unique())}
+    ds['Disaster Subtype'] = ds['Disaster Subtype'].replace(type_mapping)
+
+    type_mapping = {type_str: idx + 1 for idx, type_str in enumerate(ds['ISO'].unique())}
+    ds['ISO'] = ds['ISO'].replace(type_mapping)
+
+    type_mapping = {type_str: idx + 1 for idx, type_str in enumerate(ds['Region'].unique())}
+    ds['Region'] = ds['Region'].replace(type_mapping)
+
+    type_mapping = {type_str: idx + 1 for idx, type_str in enumerate(ds['Continent'].unique())}
+    ds['Continent'] = ds['Continent'].replace(type_mapping)
+
+    ## Changing Types ##
+
+    ds['Total Deaths'] = ds['Total Deaths'].astype(int)
+    ds['No Injured'] = ds['No Injured'].astype(int)
+    ds['No Affected'] = ds['No Affected'].astype(int)
+    ds['Total Affected'] = ds['Total Affected'].astype(int)
+
+    
+
     return ds
 
 def getInfoDataSet(ds):
     ds.info()
 
-def separateValues(ds):
-    x = ds.drop(['Total Deaths'],axis=1)
+def separateValuesTD(ds):
+    x = ds.drop(['Total Deaths','Total Affected','No Affected','No Injured'],axis=1)
     y = ds['Total Deaths'].values
     return x,y
 
